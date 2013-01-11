113ead9daed2c5d55d20c6fe9eef43fb8ed61424
= <=>
2013-01-11 11:53:15 -0800

    first commit

---
+++ komanda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Komanda::Application.config.secret_token = '6a8522462e4976d7bf0adb1b1760951209c65d8b73ecbcc9fd65786a780c6fd35d9b5d1405ccaa5e73c9a072acf832993a7850e8daa3b13afeb1161b869625d0'
